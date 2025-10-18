2b4faacf10d82a6a311f973150c353a5231098b5
achrafmz <achrafmazouz50@gmail.com>
2025-10-18 11:00:19 +0100

    first cmt

---
+++ src/main/java/com/example/entities/Salle.java
@@ -0,0 +1,49 @@
+package com.example.entities;
+
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "salles")
+public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+
+    @OneToMany(mappedBy = "salle", fetch = FetchType.EAGER)
+    private List<Machine> machines;
+
+    public Salle(String a101) {
+    }
+
+    public Salle() {
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setMachines(List<Machine> machines) {
+        this.machines = machines;
+    }
+// Constructeurs, getters et setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public List<Machine> getMachines() {
+        return machines;
+    }
+}
